8669290d5990f0f903579ee11a2543c28f01dbee
Mahalingam <sundarraj.mahalingam@capgemini.com>
2024-05-18 17:56:39 +0530

    pushed by sundarraj

---
+++ src/com/contains/ContainsMethodInString.java
@@ -0,0 +1,14 @@
+package com.contains;
+
+public class ContainsMethodInString {
+
+	public static void main(String[] args) {
+		
+		String str1 = "Welcome you all to Capgemini";
+		String str2 = "all";
+		
+		System.out.println(str1.contains(str2));
+
+	}
+
+}
